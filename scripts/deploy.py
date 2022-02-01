61cef231072da2e6b5746f6dff9594419e71e9b7
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-02-01 16:06:46 +0530

    Update deploy.py

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,7 +3,28 @@ import os, time
 
 
 def deploySimpleStorage():
-    pass
+    # four methods to use sensitive variables using these techniques
+    account = accounts[0]
+    # account = accounts.load("myAccount")
+    # account = accounts.add(os.getenv("PRIVATE_KEY"))
+    # account = accounts.add(config["wallets"]["from_key"])
+    # print(account)
+
+    # deploying contract
+    simpleDeploy = SimpleStorage.deploy({"from": account})
+
+    # initializing retrieve and getting 0 since we initialized it with 0
+    storedValue = simpleDeploy.retrieve()
+    print(storedValue)
+
+    # updating the store function and getting 1
+    transaction = simpleDeploy.store(1, {"from": account})
+    storedValue = simpleDeploy.retrieve()
+    print(storedValue)
+
+    # waiting for all transactions to finish
+    # time.sleep(1)
+    transaction.wait(1)
 
 
 def main():
