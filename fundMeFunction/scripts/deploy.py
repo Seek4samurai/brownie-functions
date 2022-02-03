98edad0d8c3ffc17b3599b3f8dbce7a518681b4d
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-02-03 19:55:21 +0530

    setting up deployMock function and aiding through helpingScripts.py

--- fundMeFunction/scripts/deploy.py
+++ fundMeFunction/scripts/deploy.py
@@ -1,13 +1,22 @@
-from brownie import FundMe, network, config
-from scripts.helpingScript import get_account
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpingScript import get_account, deployMocks
 
 
 def deployFundMe():
     account = get_account()
-    if network.show_active != "development":
-        priceFeedAddress = config["networks"][network.show_active()]["ethUsdPriceFeed"]
-    
-    fundMe = FundMe.deploy(priceFeedAddress, {"from": account}, publish_source=True)
+    if network.show_active() != "development":
+        priceFeedAddress = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deployMocks()
+        priceFeedAddress = MockV3Aggregator[-1].address
+
+    fundMe = FundMe.deploy(
+        priceFeedAddress,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
     print(f"Contract deployed to {fundMe.address}")
 
 
--- fundMeFunction/scripts/helpingScript.py
+++ fundMeFunction/scripts/helpingScript.py
@@ -1,7 +1,16 @@
-from brownie import network, config, accounts
+from brownie import network, config, accounts, MockV3Aggregator
+from web3 import Web3
 
 def get_account():
-  if (network.show_active() == "development"):
-    return accounts[0]
-  else:
-    return accounts.add(config["wallets"]["from_key"])
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
+
+def deployMocks():
+    print(f"Active network is {network.show_active()}")
+    ("Deploying Mocks...")
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(18, Web3.toWei(2000, "ether"), {"from": get_account()})
+    print("Mock Deployed!")
